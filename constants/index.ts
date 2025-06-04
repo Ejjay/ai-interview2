1e1c4af9e84c9fa17daacbb6c5409c285af1e26e
ejjays <christsonalloso021@gmail.com>
2025-06-05 00:16:13 +0800

    change to adam

--- constants/index.ts
+++ constants/index.ts
@@ -205,7 +205,7 @@ export const interviewer: CreateAssistantDTO = {
   },
   voice: {
     provider: "11labs",
-    voiceId: "sarah",
+    voiceId: "adam",
     stability: 0.4,
     similarityBoost: 0.8,
     speed: 0.9,
